32e72e5b29afc6bdfeeab58c3a3aebddc00cc3f8
HugoLorent <hugo.lorent@outlook.com>
2022-12-07 19:51:31 +0100

    finish day-2
    Signed-off-by: HugoLorent <hugo.lorent@outlook.com>

--- day-2.ts
+++ day-2.ts
@@ -1,13 +1,7 @@
-const getRockPaperScissorsScoreForAllRounds = (rounds: string) => {
-  let score = 0;
-  rounds.split('\n').forEach((round) => {
-    round = round.replaceAll(' ', '');
-    score += getRoundScore(round[0], round[1]);
-  });
-  return score;
-};
-
-const getRoundScore = (opponentChoose: string, myChoose: string): number => {
+const getRoundScoreFirstPart = (
+  opponentChoose: string,
+  myChoose: string
+): number => {
   let score: number = 0;
   if (myChoose === 'X') {
     score += 1;
@@ -26,6 +20,40 @@ const getRoundScore = (opponentChoose: string, myChoose: string): number => {
     return score;
   }
   return score;
+};
+
+const getRoundScoreSecondPart = (
+  opponentChoose: string,
+  myChoose: string
+): number => {
+  let score: number = 0;
+  if (myChoose === 'X') {
+    if (opponentChoose === 'A') return (score += 3);
+    if (opponentChoose === 'B') return (score += 1);
+    if (opponentChoose === 'C') return (score += 2);
+  } else if (myChoose === 'Y') {
+    if (opponentChoose === 'A') return (score += 4);
+    if (opponentChoose === 'B') return (score += 5);
+    if (opponentChoose === 'C') return (score += 6);
+  } else if (myChoose === 'Z') {
+    if (opponentChoose === 'A') return (score += 8);
+    if (opponentChoose === 'B') return (score += 9);
+    if (opponentChoose === 'C') return (score += 7);
+  }
+  return score;
+};
+
+const getRockPaperScissorsScoreForAllRounds = (rounds: string) => {
+  const scores: number[] = [];
+  let scoreFirstPart = 0;
+  let scoreSecondPart = 0;
+  rounds.split('\n').forEach((round) => {
+    round = round.replaceAll(' ', '');
+    scoreFirstPart += getRoundScoreFirstPart(round[0], round[1]);
+    scoreSecondPart += getRoundScoreSecondPart(round[0], round[1]);
+  });
+  scores.push(scoreFirstPart, scoreSecondPart);
+  return scores;
 };
 
 console.log(
